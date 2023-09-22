cbf97f652983198fd2f6aee1b4cf7a31a575fdf8
Xalos95 <142462852+Xalos95@users.noreply.github.com>
2023-09-21 18:57:16 -0700

    finalized objects and items

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
-let greenCharacter = document.createElement('img')
-greenCharacter.src = 'assets/green-character.gif'
-greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter)
+function newImage (url, left, bottom){
+    let addition = document.createElement('img')
+    addition.src = url
+    addition.style.position = 'fixed'
+    addition.style.left = left + 'px'
+    addition.style.bottom = bottom + 'px'
+    document.body.append(addition)
+    return addition
+}
+
+function newItem (url, left, bottom){
+    let equip = newImage(url, left, bottom)
+
+    equip.addEventListener('dblclick', function(){
+        equip.remove()
+    })
+}
+
+newImage('assets/green-character.gif', 100, 100)
+newImage('assets/pine-tree.png', 450, 200)
+newImage('assets/tree.png', 200, 300 )
+newImage('assets/pillar.png', 350, 100)
+newImage('assets/crate.png', 150, 200)
+newImage('assets/well.png', 500, 425)
+
+newItem('assets/sword.png', 500, 405)
+newItem('assests/shield.png', 165, 185)
+newItem('assets/staff.png', 600, 100)
